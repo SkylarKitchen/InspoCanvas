5f7e92f4fdae2421a355ae7bb28c024b7bcc583f
Skylar Kitchen <Skylartkitchen@gmail.com>
2025-01-04 21:33:52 -0500

    Add middleware configuration and initial setup

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,6 @@
 import { auth } from "@clerk/nextjs";
 
 export default auth;
-
 // Required for Next.js middleware
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
