4d1837419956a9da96f92332c3da9986d63ec2a8
Skylar Kitchen <Skylartkitchen@gmail.com>
2025-01-04 23:02:32 -0500

    Update board placeholders and modify board logic

--- convex/board.ts
+++ convex/board.ts
@@ -5,16 +5,8 @@ import { mutation, query } from "./_generated/server";
 const ORG_BOARD_LIMIT = 2;
 
 const images = [
-  "/placeholders/1.svg",
-  "/placeholders/2.svg",
-  "/placeholders/3.svg",
-  "/placeholders/4.svg",
-  "/placeholders/5.svg",
-  "/placeholders/6.svg",
-  "/placeholders/7.svg",
-  "/placeholders/8.svg",
-  "/placeholders/9.svg",
-  "/placeholders/10.svg",
+  "placeholders/board_icon.png",
+
 ];
 
 export const create = mutation({
